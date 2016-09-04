0e522b923f104a6bc797e8703080cec780335b6a
ruhiel_murrue <ruhiel1983@gmail.com>
2016-09-04 23:37:51 +0900

    初回コミット

--- NicoNicoPlayer/Form1.Designer.cs
+++ NicoNicoPlayer/Form1.Designer.cs
@@ -28,12 +28,94 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.button1 = new System.Windows.Forms.Button();
+			this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+			this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.tableLayoutPanel2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(3, 3);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(243, 19);
+			this.textBox1.TabIndex = 1;
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 151F));
+			this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.button1, 1, 0);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 1;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 17.45283F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(400, 34);
+			this.tableLayoutPanel1.TabIndex = 2;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(252, 3);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(118, 19);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "移動";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// tableLayoutPanel2
+			// 
+			this.tableLayoutPanel2.ColumnCount = 1;
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel1, 0, 0);
+			this.tableLayoutPanel2.Controls.Add(this.webBrowser1, 0, 1);
+			this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+			this.tableLayoutPanel2.RowCount = 2;
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel2.Size = new System.Drawing.Size(406, 317);
+			this.tableLayoutPanel2.TabIndex = 3;
+			// 
+			// webBrowser1
+			// 
+			this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.webBrowser1.Location = new System.Drawing.Point(3, 43);
+			this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+			this.webBrowser1.Name = "webBrowser1";
+			this.webBrowser1.ScriptErrorsSuppressed = true;
+			this.webBrowser1.Size = new System.Drawing.Size(400, 271);
+			this.webBrowser1.TabIndex = 3;
+			this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(406, 317);
+			this.Controls.Add(this.tableLayoutPanel2);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
+			this.tableLayoutPanel2.ResumeLayout(false);
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+		private System.Windows.Forms.WebBrowser webBrowser1;
 	}
 }
 
--- NicoNicoPlayer/Form1.cs
+++ NicoNicoPlayer/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using mshtml;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,55 @@ namespace NicoNicoPlayer
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+		private void Zoom(int zoomValue)
+		{
+			SHDocVw.IWebBrowser2 wb = (SHDocVw.IWebBrowser2)webBrowser1.ActiveXInstance;
+			SHDocVw.OLECMDID cmdZoom = (SHDocVw.OLECMDID)63;
+			Object pvaIn = zoomValue;
+			Object pvaOut = null;
+			wb.ExecWB(cmdZoom, SHDocVw.OLECMDEXECOPT.OLECMDEXECOPT_DODEFAULT, ref pvaIn, ref pvaOut);
+		}
+
+		private void Init()
+		{
+			IHTMLDocument2 doc = (webBrowser1.Document.DomDocument) as IHTMLDocument2;
+			// The first parameter is the url, the second is the index of the added style sheet.
+			IHTMLStyleSheet ss = doc.createStyleSheet("", 0);
+			this.Text = webBrowser1.DocumentTitle;
+			// Now that you have the style sheet you have a few options:
+			// 1. You can just set the content as text.
+			//ss.cssText = @"div { display: none; }";
+			// 2. You can add/remove style rules.
+			ss.addRule("div#content", "padding: 0px;");
+			ss.addRule("div#siteHeader", "display: none;");
+			ss.addRule("div#videoHeader", "display: none;");
+			ss.addRule("div#playlist", "display: none;");
+			ss.addRule("div#playlistInformation", "display: none;");
+			ss.addRule("div#playlistContainer", "display: none;");
+			ss.addRule("div#playlistTrashbox", "display: none;");
+			ss.addRule("div#videoExplorerExpand", "display: none;");
+			ss.addRule("div#bottomContentTabContainer", "display: none;");
+			ss.addRule("div#footer", "display: none;");
+			ss.addRule("div#playerTabContainer", "display: none;");
+
+			Zoom(50);
+		}
+		
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if(!textBox1.Text.Contains("http://www.nicovideo.jp"))
+			{
+				textBox1.Text = "http://www.nicovideo.jp/watch/" + textBox1.Text;
+			}
+			
+			this.webBrowser1.Url = new Uri(textBox1.Text);
+		}
+
+		private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+		{
+			Init();
 		}
 	}
 }
